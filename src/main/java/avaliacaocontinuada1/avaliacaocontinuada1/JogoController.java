49cff65710d924f7f211a5a252e68c1643f570bd
Bruna Oliveira de Souza <jcbruna052@hotmail.com>
2020-09-06 22:35:37 -0300

    Cria endpoints

---
+++ src/main/java/avaliacaocontinuada1/avaliacaocontinuada1/JogoController.java
@@ -0,0 +1,78 @@
+package avaliacaocontinuada1.avaliacaocontinuada1;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/jogos")
+public class JogoController {
+
+    private List<Jogador> jogadores = new ArrayList<>();
+    private List<Inimigo> inimigos = new ArrayList<>();
+
+
+    @GetMapping("/jogadores")
+    public ResponseEntity listarJogadores() {
+        if(jogadores.isEmpty()) {
+            return ResponseEntity.status(204).build();
+        } else {
+            return ResponseEntity.ok(jogadores);
+        }
+    }
+
+    @GetMapping("/inimigos")
+    public ResponseEntity listarInimigos() {
+        if(inimigos.isEmpty()) {
+            return ResponseEntity.status(204).build();
+        } else {
+            return ResponseEntity.ok(inimigos);
+        }
+    }
+
+    @PostMapping("/criarJogador")
+    public ResponseEntity criarJogador (@RequestBody Jogador jogador) {
+        jogadores.add(jogador);
+        return ResponseEntity.status(201).build();
+    }
+
+    @PostMapping("/criarInimigo")
+    public ResponseEntity criarInimigo (@RequestBody Inimigo inimigo) {
+        inimigos.add(inimigo);
+        return ResponseEntity.status(201).build();
+    }
+
+    @PostMapping("/concederExp/{id}/{exp}")
+    public ResponseEntity concederExp (@PathVariable int id, @PathVariable int exp) {
+        Jogador j = jogadores.get(id);
+
+        if (j != null) {
+            j.setExp(exp);
+        }
+
+        return ResponseEntity.ok(j);
+    }
+
+    @DeleteMapping("/matarInimigo/{id}")
+    public ResponseEntity matarInimigo (@PathVariable int id) {
+        if(jogadores.size() >= id) {
+            jogadores.remove(id - 1);
+            return ResponseEntity.ok().build();
+        } else {
+            return  ResponseEntity.status(404).build();
+        }
+    }
+
+    @DeleteMapping("/matarJogador/{id}")
+    public ResponseEntity matarJogador (@PathVariable int id) {
+        if(jogadores.size() >= id) {
+            jogadores.remove(id - 1);
+            return ResponseEntity.ok().build();
+        } else {
+            return  ResponseEntity.status(404).build();
+        }
+    }
+}
